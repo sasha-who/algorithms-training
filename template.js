5d2d8ccada61f0cbc87512cbca9fc0a08925c08e
Sasha Smygina <50270566+sasha-who@users.noreply.github.com>
2022-06-04 20:13:05 +0300

    Update template

--- template.js
+++ template.js
@@ -4,6 +4,6 @@ const getResult = (data) => {
 
 const fs = require("fs");
 const fileContent = fs.readFileSync("input.txt", "utf8");
-const data = fileContent.toString().split("\n");
+const data = fileContent.toString().trim().split("\n");
 
 fs.writeFileSync("output.txt", getResult(data).toString());
